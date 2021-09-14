7bef00a9c93cf544962a4684a1bb471edfeceda9
Mustafa Bodur <l1115026@std.yildiz.edu.tr>
2021-09-14 10:21:33 +0300

    Add files via upload
    Fix for include both stubs and mocks.

--- main.c
+++ main.c
@@ -4,12 +4,11 @@ void test(void) {
     puts("This is original.");
 }
 
-
 void caller(void) {
     test();
 }
 
 int main(int argc, char ** argv) {
-    test();
+    caller();
     return 0;
 }
--- mock.h
+++ mock.h
@@ -1,10 +1,17 @@
 #ifndef __MOCK_H
 #define __MOCK_H
 
+#define RET(x) IS_PROBE( _NOT_ ## x )
+#define IS_PROBE(...) SECOND(__VA_ARGS__, return)
+#define SECOND(a, b, ...) b
+#define _NOT_void PROBE()
+#define PROBE() ~, 
+
+
 /* macro to initialize mock before use */
 #define mock_func_init(type, func, ...) \
 static type (*func##__)(__VA_ARGS__) = NULL; \
-type func(__VA_ARGS__) { func##__(); }
+type func(__VA_ARGS__) { RET(type) func##__(__VA_ARGS__); }
 
 /* macro to set which mock func to use */
 #define mock_set(func, mock) \
